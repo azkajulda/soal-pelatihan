8229be192b8f6040efd53a1f4a90ac74b0d55fc6
azkajulda <azkajulda@gmail.com>
2019-06-19 09:50:40 +0700

    fix login and regist with NIK

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,8 +7,8 @@
             <form method="POST" action="{{ route('login') }}">
                 @csrf
                 <div class="input">
-                    <label for="name">Email</label>
-                    <input type="email" id="name" class="form-control @error('email') is-invalid @enderror" name="email" required autocomplete="email" autofocus>
+                    <label for="name">NIK</label>
+                    <input type="number" id="name" class="form-control @error('email') is-invalid @enderror" name="email" required autocomplete="email" autofocus>
                     <span class="spin"></span>
                 </div>
                 @error('email')
@@ -40,8 +40,8 @@
             <form method="POST" action="{{ route('register') }}">
                 @csrf
                 <div class="input">
-                    <label for="regname">Email</label>
-                    <input id="regname" type="email" class="form-control @error('email') is-invalid @enderror" name="email" required autocomplete="email">
+                    <label for="regname">NIK</label>
+                    <input id="regname" type="number" class="form-control @error('email') is-invalid @enderror" name="email" required autocomplete="email">
                     <span class="spin"></span>
                 </div>
                 @error('email')
@@ -84,12 +84,11 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="number" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
